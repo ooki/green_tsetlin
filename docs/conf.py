af3e76a4b22c15c009ef84906713083a303adadc
Eirik Vinje <eirik.matias@gmail.com>
2024-05-10 13:42:18 +0200

    trying to fix only class names showing

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ nbsphinx_execute = "never"
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = ["ROOT"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
