0a8a360fc64f4df0ed80a0683dac7f2d2db24cdf
Sebastianostby <sebastiaos@uia.no>
2025-06-28 15:36:17 +0200

    add predictor test for trec and mnist

--- setup.py
+++ setup.py
@@ -107,7 +107,8 @@ setup(
           'scipy >= 1.10.1',
           'scikit-learn >= 1.2',
           'tqdm >= 4.65',
-          'optuna'
+          'optuna',
+          'datasets>=3.6.0'
       ],
     tests_require=['pytest'],
     test_suite='tests',
